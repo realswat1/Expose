0ea448bc86d0c8094dcd89b144c3d557c3bf64ac
Godfred Asamoah <realswat@meta.com>
2023-07-05 22:37:16 -0700

    Revert "start with user creation and login backend"
    This reverts commit c71f99732a8ea07b20518a36f98e871e7ed2b40e.

--- expose-app-api/database.js
+++
@@ -1,7 +0,0 @@
-// database.js
-import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize('Exposedatabase', 'exposeswat', 'iamtheSwat1*', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
